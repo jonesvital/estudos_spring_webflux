46bd14d08d617f5d2bb117bdd9a530ac1f183efd
jones <jonesvital@gmail.com>
2022-10-24 22:35:33 -0300

    first commit

---
+++ src/test/java/estudos/spring_webflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.spring_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
